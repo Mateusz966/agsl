b2fd4f750a668f5aed94515aba17aea931aa7ba4
mateusz <mateusz.szuber@revolve.pro>
2023-02-09 23:35:23 +0100

    bump class-validator version, create env, remove usage of hardcoded secrets

--- server/mikro-orm.config.ts
+++ server/mikro-orm.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from '@mikro-orm/postgresql';
+import { databaseConfig } from '@config/database.config';
 
 export default defineConfig({
   entities: ['dist/**/*.model.js'],
   entitiesTs: ['src/**/*.model.ts'],
-  dbName: 'postgres',
-  user: 'postgres',
-  password: 'postgres',
-  port: 5666,
-  type: 'postgresql',
+  ...databaseConfig,
 });
