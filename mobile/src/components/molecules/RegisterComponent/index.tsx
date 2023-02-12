557617ad51da4a5966db8b108b06dee1252883c3
Victoria Budziak <victoria.budziak@revolve.pro>
2023-02-12 17:41:47 +0100

    [create-flow-register] Create controlled text input component

--- mobile/src/components/molecules/RegisterComponent/index.tsx
+++ mobile/src/components/molecules/RegisterComponent/index.tsx
@@ -2,15 +2,25 @@ import React, {memo} from 'react';
 import {View} from 'react-native';
 import Button from '../../atoms/Button';
 import TextInput from '../../atoms/TextInput';
+import {useForm} from 'react-hook-form';
+import {Text} from 'react-native-paper';
+import client from '../../../api/client';
+import ControlledTextInput from '../ControlledTextInput';
 
-const RegisterComponent = () => (
-  <View>
-    <TextInput label="nick" />
-    <TextInput label="email" />
-    <TextInput label="password" />
-    <Button mode="contained" onPress={() => {}}>
-      Register
-    </Button>
-  </View>
-);
-export default memo(RegisterComponent);
+const RegisterComponent = () => {
+  const {handleSubmit, control} = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <View>
+      <ControlledTextInput control={control} name="nick" />
+
+      <ControlledTextInput control={control} name="email" />
+
+      <ControlledTextInput control={control} name="password" />
+
+      <Button onPress={handleSubmit(onSubmit)}>Login in</Button>
+    </View>
+  );
+};
+export default RegisterComponent;
