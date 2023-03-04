559d9cfba45cca9c49df2918b9a2db931aae0fa6
Victoria Budziak <victoria.budziak@revolve.pro>
2023-03-04 15:42:35 +0100

    [create-register-flow] Add api routes object

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
-import {AXIOS_CONFIGURATION} from './const';
+import {API_ROUTES, AXIOS_CONFIGURATION} from './const';
 import {RegisterRequest, SignInRequest, SignInResponse} from './types';
 
 const httpClient = axios.create(AXIOS_CONFIGURATION);
 
 export const signUpUser = async (user: RegisterRequest) => {
-  const response = await httpClient.post('/users', user);
+  const response = await httpClient.post(API_ROUTES.v1.register, user);
   return response.data;
 };
 
 export const loginUser = async (user: SignInRequest) => {
   const response = await httpClient.post<SignInResponse>(
-    '/auth/mobile/sign-in',
+    API_ROUTES.v1.register,
     user,
   );
   return response.data;
--- mobile/src/api/const.ts
+++ mobile/src/api/const.ts
@@ -1,4 +1,8 @@
+export const API_ROUTES = {
+  v1: {register: '/v1/user', login: '/v1/auth/mobile/sign-in'},
+};
+
 export const AXIOS_CONFIGURATION = {
-  baseURL: 'http://10.0.2.2:3000/v1',
+  baseURL: 'http://10.0.2.2:3000/',
   timeout: 10000,
 };
