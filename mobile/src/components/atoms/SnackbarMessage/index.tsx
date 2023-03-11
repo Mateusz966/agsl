2b199872d7d39a7241ef4d6d528e93c8fd3c8774
Victoria Budziak <victoria.budziak@revolve.pro>
2023-03-11 12:30:43 +0100

    [create-register-flow] Upadte SnackBar styling

--- mobile/src/components/atoms/SnackbarMessage/index.tsx
+++ mobile/src/components/atoms/SnackbarMessage/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Snackbar} from 'react-native-paper';
 import {DEFAULT_DURATION} from './const';
+import {styles} from './styles';
 import {SnackbarMessageProps} from './types';
 
 const SnackbarMessage = ({visible, text, onDismiss}: SnackbarMessageProps) => (
   <Snackbar
     visible={visible}
+    wrapperStyle={styles.container}
     duration={DEFAULT_DURATION}
     onDismiss={onDismiss}
     action={{
