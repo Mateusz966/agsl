6bbeac47bc9fc2ac15a4c37d0bf0f07fa4b42dd5
Victoria <victoria.sobieraj.98@gmail.com>
2023-09-15 16:10:04 +0200

    [mobile-dish-form-view] Add navigation header option

--- mobile/src/navigators/index.tsx
+++ mobile/src/navigators/index.tsx
@@ -4,13 +4,26 @@ import LoginPage from '../components/pages/LoginPage';
 import AddDish from '../components/pages/AddDish';
 import {Scenes} from './const';
 import {RootStackParamList} from './types';
+import {
+  addDishHeaderOptions,
+  loginPageHeaderOptions,
+  mainHeaderOptions,
+} from './headerOptions';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Navigation = () => (
-  <Stack.Navigator>
-    <Stack.Screen name={Scenes.Login} component={LoginPage} />
-    <Stack.Screen name={Scenes.AddDish} component={AddDish} />
+  <Stack.Navigator screenOptions={mainHeaderOptions}>
+    <Stack.Screen
+      name={Scenes.Login}
+      component={LoginPage}
+      options={loginPageHeaderOptions}
+    />
+    <Stack.Screen
+      name={Scenes.AddDish}
+      component={AddDish}
+      options={addDishHeaderOptions}
+    />
   </Stack.Navigator>
 );
 export default Navigation;
