2b5222a49c34a044bf0cc51fb21517c1e06e10cb
Victoria <victoria.sobieraj.98@gmail.com>
2023-09-21 22:31:10 +0200

    [mobile-dish-form-view] Remove unused component

--- mobile/src/components/molecules/List/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {List as PaperList, ListSectionProps} from 'react-native-paper';
-
-const List = ({children}: ListSectionProps) => {
-  return <PaperList.Section>{children}</PaperList.Section>;
-};
-export default List;
