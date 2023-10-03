bfd1334ece32456a345552698da89c1d472b2129
Victoria <victoria.sobieraj.98@gmail.com>
2023-10-03 21:48:21 +0200

    [mobile-dish-form-view] Change dish units

--- mobile/src/components/organisms/IngredientsFieldsArray/types.ts
+++ mobile/src/components/organisms/IngredientsFieldsArray/types.ts
@@ -1,7 +1,14 @@
+import {Control} from 'react-hook-form';
 import {Unit} from '../../../api/dish/types';
+import {AddDish} from '../DishForm/validation';
 
 export const DISH_UNITS = [
   {label: Unit.g, value: 'g'},
   {label: Unit.ml, value: 'ml'},
-  {label: Unit.portion, value: 'portion'},
+  {label: Unit.kg, value: 'kg'},
+  {label: Unit.l, value: 'l'},
 ];
+
+export interface IngredientsFieldsArrayProps {
+  control: Control<AddDish>;
+}
