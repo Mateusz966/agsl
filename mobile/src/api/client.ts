2114bbdcda3b681631fbaeaf537641547cacf976
Victoria <victoria.sobieraj.98@gmail.com>
2023-10-09 17:30:19 +0200

    [mobile-dish-form-view] Add bearer token to axios header

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -1,4 +1,25 @@
 import axios from 'axios';
-import {AXIOS_CONFIGURATION} from './const';
+import {API_ROUTES, AXIOS_CONFIGURATION} from './const';
+import * as Keychain from 'react-native-keychain';
 
 export const httpClient = axios.create(AXIOS_CONFIGURATION);
+
+httpClient.interceptors.request.use(
+  async config => {
+    console.log(config);
+    if (config.url === API_ROUTES.v1.register) {
+      delete config.headers.Authorization;
+    } else {
+      if (!config.headers.Authorization) {
+        const credentials = await Keychain.getGenericPassword();
+        if (credentials && credentials.password) {
+          config.headers.Authorization = `Bearer ${credentials.password}`;
+        }
+      }
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
