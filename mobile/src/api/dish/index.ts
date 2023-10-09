64e86644fa217ff8c4b628953501d9dc9163b73a
Victoria <victoria.sobieraj.98@gmail.com>
2023-10-09 17:33:06 +0200

    [mobile-dish-form-view] Convert ingredients list to json

--- mobile/src/api/dish/index.ts
+++ mobile/src/api/dish/index.ts
@@ -3,6 +3,11 @@ import {API_ROUTES} from '../const';
 import {DishRequest} from './types';
 
 export const addDish = async (dish: DishRequest) => {
-    const response = await httpClient.post(API_ROUTES.v1.addDish, dish);
-    return response.data;
+  const {ingredients, name, photo} = dish;
+  const response = await httpClient.post(API_ROUTES.v1.addDish, {
+    ingredients: JSON.stringify(ingredients),
+    name,
+    photo,
+  });
+  return response.data;
 };
