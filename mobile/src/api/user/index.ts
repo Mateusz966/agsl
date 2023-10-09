af4b54c543397612831f9399e82478efa1981f5a
Victoria <victoria.sobieraj.98@gmail.com>
2023-10-09 17:32:42 +0200

    [mobile-dish-form-view] Save token after login

--- mobile/src/api/user/index.ts
+++ mobile/src/api/user/index.ts
@@ -1,9 +1,11 @@
 import {httpClient} from '../client';
 import {API_ROUTES} from '../const';
 import {RegisterRequest, SignInRequest, SignInResponse} from './types';
+import * as Keychain from 'react-native-keychain';
 
 export const signUpUser = async (user: RegisterRequest) => {
   const response = await httpClient.post(API_ROUTES.v1.register, user);
+
   return response.data;
 };
 
@@ -12,5 +14,8 @@ export const loginUser = async (user: SignInRequest) => {
     API_ROUTES.v1.login,
     user,
   );
+  const {accessToken} = response.data;
+  await Keychain.setGenericPassword('password', accessToken);
+
   return response.data;
 };
