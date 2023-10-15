141a9dc6b1985757cd3ddd5c5fa5ab579ab836f6
Victoria <victoria.sobieraj.98@gmail.com>
2023-10-15 19:03:31 +0200

    [mobile-dish-form-view] Add icon paths to utils

--- mobile/src/components/organisms/IngredientsFieldsArray/index.tsx
+++ mobile/src/components/organisms/IngredientsFieldsArray/index.tsx
@@ -2,33 +2,40 @@ import React, {FC, memo} from 'react';
 import Button from '../../atoms/Button';
 import ControlledTextInput from '../../molecules/ControlledInputs/ControlledTextInput';
 import {useFieldArray} from 'react-hook-form';
-import {ScrollView, View} from 'react-native';
+import {View} from 'react-native';
 import ControlledSelect from '../../molecules/ControlledInputs/ControlledSelect';
 import {Unit} from '../../../api/dish/types';
-import {ButtonType} from '../../atoms/Button/types';
-
 import styles from './styles';
 import {DISH_UNITS, IngredientsFieldsArrayProps} from './types';
+import {ICON_PATHS} from '../../../utils/icons';
 
-const IngredientsFieldsArray: FC<IngredientsFieldsArrayProps> = ({control}) => {
+const IngredientsFieldsArray: FC<IngredientsFieldsArrayProps> = ({form}) => {
+  const {
+    control,
+    formState: {errors},
+  } = form;
+
   const {fields, append, remove} = useFieldArray({
     control,
     name: 'ingredients',
   });
 
   return (
-    <ScrollView>
+    <View style={styles.scrollContainer}>
       {fields.map((_, index) => (
         <View style={styles.addIngredientContainer} key={index}>
           <ControlledTextInput
             name={`ingredients.${index}.name`}
             control={control}
             displayName="Nazwa"
+            error={errors?.ingredients?.[index]?.name?.message}
           />
           <ControlledTextInput
             name={`ingredients.${index}.amount`}
             control={control}
             displayName="Ilość"
+            keyboardType="numeric"
+            error={errors?.ingredients?.[index]?.amount?.message}
           />
           <ControlledSelect
             control={control}
@@ -37,20 +44,19 @@ const IngredientsFieldsArray: FC<IngredientsFieldsArrayProps> = ({control}) => {
             name={`ingredients.${index}.unit`}
           />
           <Button
-            icon={require('mobile/src/assets/icons/trash.png')}
+            icon={ICON_PATHS.TRASH_ICON}
             onPress={() => remove(index)}
             style={styles.deleteButton}
           />
         </View>
       ))}
       <Button
-        type={ButtonType.Secondary}
-        icon={require('mobile/src/assets/icons/add.png')}
+        icon={ICON_PATHS.ADD_ICON}
         style={styles.addButton}
         onPress={() => append({name: '', amount: 0, unit: Unit.g})}>
         Add another ingredient
       </Button>
-    </ScrollView>
+    </View>
   );
 };
 export default memo(IngredientsFieldsArray);
---
+++ mobile/src/utils/icons.ts
@@ -0,0 +1,4 @@
+export const ICON_PATHS = {
+  TRASH_ICON: require('mobile/src/assets/icons/trash.png'),
+  ADD_ICON: require('mobile/src/assets/icons/add.png'),
+};
