6cb89193da983602543483565833eecf7f35744a
Victoria <victoria.sobieraj.98@gmail.com>
2023-10-22 17:04:39 +0200

    [display-dish-list]  Mofidy dish card component

--- mobile/src/components/molecules/DishCard/index.tsx
+++ mobile/src/components/molecules/DishCard/index.tsx
@@ -1,32 +1,39 @@
 import React, {FC} from 'react';
-import {Card} from 'react-native-paper';
+import {Avatar, Card} from 'react-native-paper';
 import {memo} from 'react';
 import Message from '../../atoms/Message';
 import {DishCardProps} from './types';
-import Photo from '../../atoms/Photo';
-import {Pressable, View} from 'react-native';
+import {View} from 'react-native';
 import styles from './styles';
-import ActionButtonsContainer from '../ActionButtonsContainer';
+import {ICON_PATHS} from '../../../utils/icons';
+import Button from '../../atoms/Button';
+import {components} from '../../../config/theme';
 
-const DishCard: FC<DishCardProps> = ({dishName, photoSource}) => (
-  <Pressable>
-    <Card
-      style={styles.cardBackgroundStyle}
-      elevation={1}
-      contentStyle={styles.cardContentStyle}>
-      <Photo style={styles.photoStyle} source={photoSource ?? ''} />
-      <View style={styles.dishNameContainer}>
-        <Message style={styles.textStyle} message={dishName} />
-        <ActionButtonsContainer
-          primaryButtonHandler={() => {}}
-          secondaryButtonHandler={() => {}}
-          primaryButtonText="Edit"
-          secondaryButtonText="Delete"
-          containerStyle={styles.buttonContainer}
-        />
-      </View>
-    </Card>
-  </Pressable>
+const DishCard: FC<DishCardProps> = ({
+  dishName,
+  photoSource,
+  onPressHandler,
+}) => (
+  <Card
+    style={styles.cardBackgroundStyle}
+    elevation={5}
+    contentStyle={styles.cardContentStyle}>
+    <Avatar.Icon
+      style={styles.photoStyle}
+      size={100}
+      icon={photoSource ?? ICON_PATHS.CAMERA}
+    />
+    <View style={styles.dishNameContainer}>
+      <Message style={styles.textStyle} message={dishName} />
+      <Button
+        onPress={onPressHandler}
+        style={styles.buttonStyle}
+        textColor={components.button.textColor.primary}
+        icon={ICON_PATHS.EDIT}>
+        Edit
+      </Button>
+    </View>
+  </Card>
 );
 
 export default memo(DishCard);
