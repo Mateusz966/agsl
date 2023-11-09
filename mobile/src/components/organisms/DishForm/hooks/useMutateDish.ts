fcf185d887104a47ec79087b86e8e8d4a619ba23
Victoria <victoria.sobieraj.98@gmail.com>
2023-11-09 19:45:10 +0100

    [display-dish-list]  Add edit dish mutation

--- mobile/src/components/organisms/DishForm/hooks/useMutateDish.ts
+++ mobile/src/components/organisms/DishForm/hooks/useMutateDish.ts
@@ -4,22 +4,36 @@ import {AddDish, addDishSchema} from '../validation';
 import {useMutation} from '@tanstack/react-query';
 import {DishRequest} from '../../../../api/dish/types';
 import {addDish, editDish} from '../../../../api/dish';
-import {useCallback} from 'react';
-import {RouteProp, useFocusEffect, useRoute} from '@react-navigation/native';
+import {useCallback, useMemo} from 'react';
+import {RouteProp, useRoute} from '@react-navigation/native';
 import {RootStackParamList} from '../../../../navigators/types';
 import {Scenes} from '../../../../navigators/const';
 import {useSnackbarContext} from '../../../atoms/SnackbarMessage/useSnackbarContext';
 import {EditDishRequest, UseMutateDishProps} from './types';
 import {DEFAULT_DISH_FORM_VALUE} from './const';
+import {useDishContext} from './DishContext/useDishContext';
 
 export const useMutateDish = ({img}: UseMutateDishProps) => {
   const {params} = useRoute<RouteProp<RootStackParamList, Scenes.AddDish>>();
   const {setText, setVisible} = useSnackbarContext();
+  const {setDishId} = useDishContext();
+
+  const paramsDishValue = useMemo(
+    () =>
+      params
+        ? {
+            name: params.name,
+            ingredients: params.ingredients,
+            photo: params.photo,
+          }
+        : DEFAULT_DISH_FORM_VALUE,
+    [params],
+  );
 
   const form = useForm<AddDish>({
     resolver: zodResolver(addDishSchema),
     mode: 'onChange',
-    defaultValues: DEFAULT_DISH_FORM_VALUE,
+    defaultValues: paramsDishValue,
   });
 
   const {append, fields, remove} = useFieldArray({
@@ -42,7 +56,8 @@ export const useMutateDish = ({img}: UseMutateDishProps) => {
 
   const editDishMutation = useMutation<void, void, EditDishRequest>({
     mutationFn: payload => editDish(payload.id, payload.dishData),
-    onSuccess: () => {
+    onSuccess: async () => {
+      setDishId(params?.id ?? '');
       setVisible(true);
       setText('Your dish was edited sucessfully');
     },
@@ -55,45 +70,29 @@ export const useMutateDish = ({img}: UseMutateDishProps) => {
   const onSubmit = useCallback(
     (payload: DishRequest) => {
       const fd = new FormData();
-      if (payload.photo) {
+      if (img) {
         const photo = {
-          uri: img?.uri,
-          name: img?.fileName,
-          type: img?.type,
+          uri: img.uri,
+          name: img.fileName,
+          type: img.type,
         };
         fd.append('photo', photo);
       }
       fd.append('ingredients', JSON.stringify(payload.ingredients));
       fd.append('name', payload.name);
-      addDishMutation.mutate(fd);
+
+      if (params?.id) {
+        editDishMutation.mutate({id: params.id, dishData: fd});
+      } else {
+        addDishMutation.mutate(fd);
+      }
     },
-    [addDishMutation, img?.fileName, img?.type, img?.uri],
+    [addDishMutation, editDishMutation, img, params?.id],
   );
 
   const onCancel = () => {
     form.reset();
   };
-
-  useFocusEffect(
-    useCallback(() => {
-      if (params) {
-        form.setValue('name', params.name);
-        params.ingredients.map((ingredient, index) => {
-          if (index === 0) {
-            form.setValue(`ingredients.${index}.name`, ingredient.name);
-            form.setValue(`ingredients.${index}.amount`, ingredient.amount);
-            form.setValue(`ingredients.${index}.unit`, ingredient.unit);
-          } else {
-            append({
-              name: ingredient.name,
-              amount: ingredient.amount,
-              unit: ingredient.unit,
-            });
-          }
-        });
-      }
-    }, [append, form, params]),
-  );
 
   return {
     append,
