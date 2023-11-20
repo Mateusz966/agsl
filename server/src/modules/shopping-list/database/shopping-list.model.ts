e98bb4049bba37291eddea1250595e606841feca
mat <mateuszlibero@gmail.com>
2023-11-21 00:34:55 +0100

    improved types

--- server/src/modules/shopping-list/database/shopping-list.model.ts
+++ server/src/modules/shopping-list/database/shopping-list.model.ts
@@ -1,6 +1,7 @@
 import { ModelBase } from '@libs/db/model.base';
 import { Column, Entity, ManyToOne } from 'typeorm';
 import { UserModel } from '@modules/user/database/user.model';
+import {IngredientsModel} from "@modules/dish/database/ingredients.model";
 
 @Entity({ name: 'shopping_list' })
 export class ShoppingListModel extends ModelBase {
@@ -8,7 +9,7 @@ export class ShoppingListModel extends ModelBase {
   isDraft: boolean;
 
   @Column({ type: 'jsonb' })
-  generatedShoppingList: any[];
+  generatedShoppingList: IngredientsModel[];
 
   @ManyToOne(() => UserModel, (user) => user.shoppingList)
   user: UserModel;
