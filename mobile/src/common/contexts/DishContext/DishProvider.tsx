e7002f879f2b82e27de96ab53b38b1ea10691079
Victoria <victoria.sobieraj.98@gmail.com>
2023-11-27 16:27:08 +0100

    [shopping-list-view] Add dishes list to dish context

--- mobile/src/common/contexts/DishContext/DishProvider.tsx
+++ mobile/src/common/contexts/DishContext/DishProvider.tsx
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import {DishContext} from './DishContext';
-import {DishContextProps, DishProviderProps} from './types';
+import {DishBasketElement, DishContextProps, DishProviderProps} from './types';
 
 export const DishProvider = ({children}: DishProviderProps) => {
   const [dishId, setDishId] = useState('');
+  const [dishesList, setDishesList] = useState<DishBasketElement[]>([]);
+
+  const totalDishCount = useMemo(
+    () => dishesList.reduce((sum, item) => sum + item.count, 0),
+    [dishesList],
+  );
 
   const value: DishContextProps = {
     dishId,
     setDishId,
+    dishesList,
+    setDishesList,
+    totalDishCount,
   };
 
   return <DishContext.Provider value={value}>{children}</DishContext.Provider>;
