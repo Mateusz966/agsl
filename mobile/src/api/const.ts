9f3e5ec5b52484548d994f38b91f5bd36e869180
Victoria <victoria.sobieraj.98@gmail.com>
2024-01-05 16:28:59 +0100

    [home-scene-view] Create error dict

--- mobile/src/api/const.ts
+++ mobile/src/api/const.ts
@@ -14,3 +14,21 @@ export const AXIOS_CONFIGURATION = {
     'Content-Type': 'application/json',
   },
 };
+
+export enum ErrorCode {
+  InvalidToken = 401,
+  NotFound = 404,
+  UserExists = 409,
+  ServerError = 500,
+}
+
+export const ERROR_MESSAGES: Partial<Record<ErrorCode, {}>> = {
+  [ErrorCode.InvalidToken]: 'Your token is no longer valid',
+  [ErrorCode.NotFound]: {
+    auth: 'Invalid email or password',
+    dish: 'Dish not found',
+    shoppingList: 'Shopping list not found',
+  },
+  [ErrorCode.UserExists]: 'User already exist',
+  [ErrorCode.ServerError]: 'Unknown error',
+};
