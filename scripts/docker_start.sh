a5456b7f7456e91c9bf11c2753ba694ae20ca9d7
mat <mateuszlibero@gmail.com>
2024-03-23 15:44:42 +0100

    add logging for before install script

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+for dir in ../*; do
+  echo "$dir"
+done
+
 DOCKER_COMPOSE_PATH="/usr/local/bin/docker-compose"
 
 "$DOCKER_COMPOSE_PATH" -f ../server/docker-compose.yml  up -d --remove-orphans
