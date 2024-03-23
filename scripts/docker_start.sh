2d6d98ba1f71f544037d5596682eb9de3dd02548
mat <mateuszlibero@gmail.com>
2024-03-23 15:47:08 +0100

    set pwd

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-for dir in ../*; do
-  echo "$dir"
-done
+echo "$PWD"
 
 DOCKER_COMPOSE_PATH="/usr/local/bin/docker-compose"
 
-"$DOCKER_COMPOSE_PATH" -f ../server/docker-compose.yml  up -d --remove-orphans
+"$DOCKER_COMPOSE_PATH" -f "$PWD"/../server/docker-compose.yml  up -d --remove-orphans
