603bbe184088a17633781a8d9aec82c554361e0d
mat <mateuszlibero@gmail.com>
2024-03-24 19:08:30 +0100

    docker_start

--- scripts/docker_start.sh
+++ scripts/docker_start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+export DB_PASSWORD="$DB_PASSWORD"
+export DB_USERNAME="$DB_USERNAME"
+export DB_NAME="$DB_NAME"
+export DB_PORT="$DB_PORT"
+export DB_CLIENT_USERNAME="$DB_CLIENT_USERNAME"
+export DB_CLIENT_PASSWORD="$DB_CLIENT_PASSWORD"
+export DB_CLIENT_PORT="$DB_CLIENT_PORT"
+
 DOCKER_COMPOSE_PATH="/usr/local/bin/docker-compose"
 
 "$DOCKER_COMPOSE_PATH" -f /agsl/server/docker-compose.yml up -d --remove-orphans
